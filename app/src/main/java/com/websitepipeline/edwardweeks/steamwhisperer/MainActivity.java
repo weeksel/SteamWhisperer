82acbbfd197b928d4e27ee45d679a09aec11c68a
FaeTality1337 <kristinelise.brown@gmail.com>
2015-05-28 19:17:06 -0400

    Built Project

--- app/src/main/java/com/websitepipeline/edwardweeks/steamwhisperer/MainActivity.java
+++ app/src/main/java/com/websitepipeline/edwardweeks/steamwhisperer/MainActivity.java
@@ -1,23 +1,40 @@
 package com.websitepipeline.edwardweeks.steamwhisperer;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.Button;
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import android.os.AsyncTask;
+import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.my_button).setOnClickListener(this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
@@ -35,4 +52,66 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onClick(View arg0) {
+        Button b = (Button) findViewById(R.id.my_button);
+
+
+        b.setClickable(false);
+        new LongRunningGetIO().execute();
+    }
+
+    private class LongRunningGetIO extends AsyncTask<Void, Void, String> {
+        protected String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
+            InputStream in = entity.getContent();
+
+            StringBuffer out = new StringBuffer();
+            int n = 1;
+            while (n > 0) {
+                byte[] b = new byte[4096];
+                n = in.read(b);
+
+                if (n > 0) out.append(new String(b, 0, n));
+            }
+            return out.toString();
+        }
+
+
+        @Override
+        protected String doInBackground(Void... params) {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpContext localContext = new BasicHttpContext();
+            HttpGet httpGet = new HttpGet("http://www.cheesejedi.com/rest_services/get_big_cheese.php?puzzle=1");
+            String text = null;
+            try {
+                HttpResponse response = httpClient.execute(httpGet, localContext);
+                HttpEntity entity = response.getEntity();
+                text = getASCIIContentFromEntity(entity);
+
+            } catch (Exception e) {
+                return e.getLocalizedMessage();
+            }
+
+            return text;
+        }
+
+
+        protected void onPostExecute(String results) {
+            if (results != null) {
+                EditText et = (EditText) findViewById(R.id.my_edit);
+
+                et.setText(results);
+            }
+
+            Button b = (Button) findViewById(R.id.my_button);
+
+            b.setClickable(true);
+        }
+
+    }
 }
+
+
+
+
